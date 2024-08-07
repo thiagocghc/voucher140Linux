c33e4d23e0478115de3a97517b18c00800acfa4f
Thiago Almeida <thiagoalmeida@live.com>
2024-08-07 09:11:26 -0400

    meu push da VM linux

---
+++ hello.py
@@ -0,0 +1,9 @@
+x = 10
+y = 20
+
+res = x + y
+if res > 10:
+    print("MAior que 10")
+else:
+    print("menor que 10")
+	
---
+++ repeticao.py
@@ -0,0 +1,5 @@
+i = 0
+while i < 10:
+   print(i)
+   print("Bom dia")
+   i += 1
