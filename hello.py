5003f12e3f8161b9169bfbfeeca931dc4dff26d0
Thiago Almeida <thiagoalmeida@live.com>
2024-08-09 09:31:59 -0400

    projeto HTML

--- hello.py
+++
@@ -1,9 +0,0 @@
-x = 10
-y = 20
-
-res = x + y
-if res > 10:
-    print("MAior que 10")
-else:
-    print("menor que 10")
-	
--- repeticao.py
+++
@@ -1,5 +0,0 @@
-i = 0
-while i < 10:
-   print(i)
-   print("Bom dia")
-   i += 1
